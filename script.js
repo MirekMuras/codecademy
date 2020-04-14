a726c07dbc5f098a923bc8dc0780c0f5f5628ae8
Miroslav Muras <mirekmuras@gmail.com>
2020-04-14 18:43:49 -0400

    final commit

--- script.js
+++ script.js
@@ -2,14 +2,18 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
+// random numebr function ( from 0 to 0)
 const generateTarget = () => Math.floor(Math.random() * 10);
+// absolute value function
 function difference(a,b) { return Math.abs(a-b);} 
+// alert function 
 const alertMessage = (userGuess) => {
     if (userGuess > 9 || userGuess < 0) {
       alert('Your guess number must be between 0 and 9!'); 
   }
 }
 
+// compare function
 const compareGuesses = (human,computer,secretTargetNumber) => {
 
 const userPlayer = difference (secretTargetNumber,computer);
@@ -22,7 +26,9 @@ alertMessage(human)
   } else return false ;
 }
 
+// update score after each iteration
 const updateScore=(winner) => (winner === 'human') ? humanScore += 1 : computerScore += 1;
+// update to new round
 const advanceRound = () => { currentRoundNumber += 1};
 
 
