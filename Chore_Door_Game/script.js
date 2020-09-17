cb87b26b3b7c6572e91ac00639e34da2ceff48a2
Miroslav Muras <mirekmuras@gmail.com>
2020-09-17 14:01:46 -0400

    commit

--- Chore_Door_Game/script.js
+++ Chore_Door_Game/script.js
@@ -6,18 +6,39 @@ const botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-
 const beachDoorPath ="https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg";
 const spaceDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg"
 
+let numClosedDoors = 3;
+let openDoor1,openDoor2,openDoor3 ;
+
+const randomChoreDoorGenerator = () => {
+   choreDoor = Math.floor(Math.random() * numClosedDoors);
+   if (choreDoor === 0) {
+     openDoor1 = botDoorPath;
+     openDoor2 = beachDoorPath;
+     openDoor3 = spaceDoorPath;
+   } else if (choreDoor === 1) {
+     openDoor2 = botDoorPath;
+     openDoor1 = beachDoorPath;
+     openDoor3 = spaceDoorPath;
+   } else {
+     openDoor3 = botDoorPath;
+     openDoor1 = beachDoorPath;
+     openDoor2 = spaceDoorPath;
+   }
+ }
+
 doorImage1.onclick = () => {
-  doorImage1.src = botDoorPath;
+  doorImage1.src = openDoor1;
 };
 
 doorImage2.onclick = () => {
-  doorImage2.src = beachDoorPath;
+  doorImage2.src = openDoor2;
 };
 
 doorImage3.onclick = () => {
-  doorImage3.src = spaceDoorPath;
+  doorImage3.src = openDoor3;
 };
 
+randomChoreDoorGenerator()
 
 
 
